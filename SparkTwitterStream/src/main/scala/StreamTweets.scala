54382a1dcf1103570f2f21e9e65eff54b9320f76
Guru Teja Mannava <gmb79>
2015-09-23 03:38:45 -0500

    no message

---
+++ SparkTwitterStream/src/main/scala/StreamTweets.scala
@@ -0,0 +1,57 @@
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.Seconds
+import org.apache.spark.streaming.twitter.TwitterUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+
+/**
+ * Created by mayanka on 9/22/15.
+ */
+object StreamTweets {
+
+  def main(args: Array[String]) {
+
+    val filters = args
+
+    // Set the system properties so that Twitter4j library used by twitter stream
+    // can use them to generate OAuth credentials
+
+    System.setProperty("twitter4j.oauth.consumerKey", "QTVD2zYsd9MMIHl7T31mWJ3r3")
+    System.setProperty("twitter4j.oauth.consumerSecret", "nR7Z9t6VvWWSEyFVBEBrUsdVeMwxkddJ96V1hBiI816kbrwaDc")
+    System.setProperty("twitter4j.oauth.accessToken", "175311186-d5S0jadlEdfbNjrr4Q1XmgOcqmzX9Mf0nh0xWpuj")
+    System.setProperty("twitter4j.oauth.accessTokenSecret", "kHdZsvQQuh6IKIKdLHQ6oU7s6XfOhFTM1bntT8IAU5k2j")
+
+    //Create a spark configuration with a custom name and master
+    // For more master configuration see  https://spark.apache.org/docs/1.2.0/submitting-applications.html#master-urls
+    val sparkConf = new SparkConf().setAppName("twitterspark_guru").setMaster("local[*]")
+    //creating streaming context with 2 second window
+    val ssc = new StreamingContext(sparkConf, Seconds(3))
+    //Using the streaming context, open a twitter stream (By the way you can also use filters)
+    //Stream generates a series of random tweets
+    val stream = TwitterUtils.createStream(ssc, None, filters)
+    stream.print()
+    //Map :Retrieving Hash Tag
+    val hashTags = stream.flatMap(status => status.getText.split(" ").filter(_.startsWith("#")))
+    //hashTags.print()
+
+    //Finding the hashtags on 30 sec window
+    val tophash = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(30))
+    //tophash.print()
+
+    //print popular hash tags
+    tophash.foreachRDD(rdd => {
+      val topList = rdd.take(30)
+      println("\n Popular hash tags in last 30 seconds window (%s total):".format(rdd.count()))
+      topList.foreach{case (tag, count) => println("%s (%s tweets)".format(tag, count))
+      SocketClient.sendCommandToRobot("helloo purii")
+      }
+    })
+
+
+
+    ssc.start()
+
+    ssc.awaitTermination()
+  }
+
+}
